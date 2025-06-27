071f9b231d5357d15e6ba6505390f067b78c218f
04kryzz <cr.cruzs@duocuc.cl>
2025-06-27 18:45:09 -0400

    PRUEBAS UNITARIAS

--- src/main/java/com/perfulandia/envios/model/Envio.java
+++ src/main/java/com/perfulandia/envios/model/Envio.java
@@ -1,22 +1,23 @@
 package com.perfulandia.envios.model;
 
-import jakarta.persistence.*;
-import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-@Table(name = "pedidos")
 public class Envio {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String direccionDestino;
     private String estado;
-    private LocalDate fechaEnvio;
-    private LocalDate fechaEntregaEstimada;
+    private String fechaEnvio;
+    private String fechaEntregaEstimada;
     private Long pedidoId;
 
-    // Getters y Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
@@ -26,11 +27,11 @@ public class Envio {
     public String getEstado() { return estado; }
     public void setEstado(String estado) { this.estado = estado; }
 
-    public LocalDate getFechaEnvio() { return fechaEnvio; }
-    public void setFechaEnvio(LocalDate fechaEnvio) { this.fechaEnvio = fechaEnvio; }
+    public String getFechaEnvio() { return fechaEnvio; }
+    public void setFechaEnvio(String fechaEnvio) { this.fechaEnvio = fechaEnvio; }
 
-    public LocalDate getFechaEntregaEstimada() { return fechaEntregaEstimada; }
-    public void setFechaEntregaEstimada(LocalDate fechaEntregaEstimada) { this.fechaEntregaEstimada = fechaEntregaEstimada; }
+    public String getFechaEntregaEstimada() { return fechaEntregaEstimada; }
+    public void setFechaEntregaEstimada(String fechaEntregaEstimada) { this.fechaEntregaEstimada = fechaEntregaEstimada; }
 
     public Long getPedidoId() { return pedidoId; }
     public void setPedidoId(Long pedidoId) { this.pedidoId = pedidoId; }
